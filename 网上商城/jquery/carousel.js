9714099dcd83948037183558d86ebaf2507ae43d
MengDaGuo <guoshuyuan111@sina.com>
2018-05-10 09:06:22 +0800

    jquery方法的carousel.js

---
+++ 网上商城/jquery/carousel.js
@@ -0,0 +1,23 @@
+var iNow=0;
+var timer;
+$('.carousel-btns li').on('click',function(){
+	$(this).addClass('selected').siblings().removeClass('selected');
+	$('.carousel-imgs').animate({
+		left:-960*$(this).index()
+	},1000)
+});
+run();
+$('.carousel').on('mouseover',function(){
+	clearInterval(timer);
+}).on('mouseout',function(){
+	run();
+});
+function run(){
+	timer=setInterval(function(){
+		iNow++;
+		if(iNow==$('.carousel-btns li').length){
+			iNow=0;
+		}
+		$('.carousel-btns li').eq(iNow).trigger('click');
+	},2000)
+}
