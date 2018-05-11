356f1841ed78d906ebdd4876f6a19369fdd50b46
MengDaGuo <guoshuyuan111@sina.com>
2018-05-11 15:40:38 +0800

    jquery练习

--- 网上商城/jquery/shop_product.js
+++ 网上商城/jquery/shop_product.js
@@ -29,3 +29,37 @@ $('.btns-box li').on('click',function(){
 	$(this).addClass('active').siblings().removeClass('active');
 	$('.tab-item').eq($(this).index()).show().siblings().hide();
 });
+
+
+// 弹层
+var iNow;
+$('.small-img li').on('click',function(){
+	$('.dialog-box').show();
+	$('.content').animate({
+		top:'10%'
+	},1000)
+	$('.dialog-body img').attr('src',$(this).find('img').attr('src'));
+	iNow=$(this).index();
+});
+
+$('.dialog-box').on('click',function(e){
+	if(e.target==$('.dialog-box')[0] || e.target==$('.dialog-close')[0]){
+		$(this).hide();
+		$('.content').stop().css({
+			top:'-250px'
+		})
+	}
+
+});
+
+$('.dialog-next').on('click',function(){
+	iNow++;
+	if(iNow==$('.small-img li').length){
+		iNow=0;
+	}
+	var $nextImgSrc=$('.small-img li').eq(iNow).find('img').attr('src');
+	$('.dialog-body img').eq(0).before('<img src="'+$nextImgSrc+'"></img>');
+	$('.dialog-body img').eq(1).fadeOut(2000,function(){
+		$(this).remove();
+	})
+})
